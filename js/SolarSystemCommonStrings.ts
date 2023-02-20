f5233110c4df1da530b2106a3ce68389204779b1
Jonathan Olson <jonathan.olson@colorado.edu>
2023-02-20 14:08:09 -0700

    Initial commit

---
+++ js/SolarSystemCommonStrings.ts
@@ -0,0 +1,15 @@
+// Copyright 2020-2022, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+
+type StringsType = {
+
+};
+
+const SolarSystemCommonStrings = getStringModule( 'AREA_MODEL_COMMON' ) as StringsType;
+
+export default SolarSystemCommonStrings;
