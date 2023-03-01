28be52971f9ba52cf0f261e6f21f93a3054eda30
phet-dev <phetadmin@gmail.com>
2023-03-01 05:13:00 -0700

    update copyright dates from daily grunt work

--- js/SolarSystemCommonQueryParameters.ts
+++ js/SolarSystemCommonQueryParameters.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2023, University of Colorado Boulder
 
 /**
  * SolarSystemCommonQueryParameters defines query parameters that are specific to this simulation.
