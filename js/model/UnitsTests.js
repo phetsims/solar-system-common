1ca348f568f443cb2d69329598907d37b7ea72cd
AgustinVallejo <agustin.vallejo@udea.edu.co>
2023-03-20 13:37:34 -0500

    Fixing unit test, see https://github.com/phetsims/my-solar-system/issues/116

--- js/model/UnitsTests.js
+++ js/model/UnitsTests.js
@@ -7,7 +7,7 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-import { G, G_ACTUAL, MASS_MULTIPLIER, POSITION_MULTIPLIER, TIME_MULTIPLIER } from '../SolarSystemCommonConstants.js';
+import { G, G_ACTUAL, MASS_MULTIPLIER, METERS_IN_AU, POSITION_MULTIPLIER, SECONDS_IN_A_YEAR, TIME_MULTIPLIER } from '../SolarSystemCommonConstants.js';
 
 QUnit.module( 'UnitsTests' );
 
@@ -22,13 +22,13 @@ QUnit.test( 'Test units', assert => {
 
   const mass1SimUnits = mass1 / MASS_MULTIPLIER;
   const mass2SimUnits = mass2 / MASS_MULTIPLIER;
-  const distanceSimUnits = distance / POSITION_MULTIPLIER;
+  const distanceSimUnits = distance / METERS_IN_AU / POSITION_MULTIPLIER;
   const forceSimUnits = G * mass1SimUnits * mass2SimUnits / ( distanceSimUnits * distanceSimUnits );
 
   // convert forceSimUnits to SI
-  const convertedToSI = forceSimUnits * MASS_MULTIPLIER * POSITION_MULTIPLIER / TIME_MULTIPLIER / TIME_MULTIPLIER;
+  const convertedToSI = forceSimUnits * MASS_MULTIPLIER * POSITION_MULTIPLIER * METERS_IN_AU / TIME_MULTIPLIER / TIME_MULTIPLIER / SECONDS_IN_A_YEAR / SECONDS_IN_A_YEAR;
   console.log( convertedToSI );
 
-  // assert.ok( Math.abs( force - convertedToSI ) < 1e-4, 'force should be the same' );
+  assert.ok( Math.abs( force - convertedToSI ) < 1e-10, 'force should be the same' );
 } );
 
