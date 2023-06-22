a3c7ce109503c527bf39892c1f4b8efa97d66090
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-06-22 17:32:17 -0600

    convert to Disposable.assertNotDisposable(), https://github.com/phetsims/axon/issues/436

--- js/view/SolarSystemCommonNumberControl.ts
+++ js/view/SolarSystemCommonNumberControl.ts
@@ -6,6 +6,7 @@
  * @author Agustín Vallejo (PhET Interactive Simulations)
  */
 
+import Disposable from '../../../axon/js/Disposable.js';
 import optionize, { EmptySelfOptions } from '../../../phet-core/js/optionize.js';
 import Dimension2 from '../../../dot/js/Dimension2.js';
 import Range from '../../../dot/js/Range.js';
@@ -89,7 +90,7 @@ export default class SolarSystemCommonNumberControl extends NumberControl {
   }
 
   public override dispose(): void {
-    assert && assert( false, 'dispose is not supported, exists for the lifetime of the sim' );
+    Disposable.assertNotDisposable();
     super.dispose();
   }
 }
