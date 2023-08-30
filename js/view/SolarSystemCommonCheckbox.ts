b8b178ef7a295b13cf961fa025bc79e0dce71853
pixelzoom <cmalley@pixelzoom.com>
2023-08-30 15:05:21 -0600

    safe combineOptions, delete duplicated options values

--- js/view/SolarSystemCommonCheckbox.ts
+++ js/view/SolarSystemCommonCheckbox.ts
@@ -7,7 +7,6 @@
  */
 
 import Checkbox, { CheckboxOptions } from '../../../sun/js/Checkbox.js';
-import SolarSystemCommonColors from '../SolarSystemCommonColors.js';
 import { combineOptions } from '../../../phet-core/js/optionize.js';
 import { Node } from '../../../scenery/js/imports.js';
 import Property from '../../../axon/js/Property.js';
@@ -17,14 +16,11 @@ import SolarSystemCommonConstants from '../SolarSystemCommonConstants.js';
 export default class SolarSystemCommonCheckbox extends Checkbox {
 
   public constructor( property: Property<boolean>, content: Node, providedOptions?: CheckboxOptions ) {
-    super( property, content, combineOptions<CheckboxOptions>( {
-      boxWidth: 14,
-      checkboxColor: SolarSystemCommonColors.foregroundProperty,
-      checkboxColorBackground: SolarSystemCommonColors.backgroundProperty,
+    super( property, content, combineOptions<CheckboxOptions>( {}, SolarSystemCommonConstants.CHECKBOX_OPTIONS, {
       touchAreaXDilation: 5,
       touchAreaYDilation: SolarSystemCommonConstants.CHECKBOX_SPACING / 2,
       containerTagName: 'div' // Just for easier visualization in a11y view
-    }, providedOptions, SolarSystemCommonConstants.CHECKBOX_OPTIONS ) );
+    }, providedOptions ) );
   }
 }
 
