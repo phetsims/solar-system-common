a4cc863dcce6d4c8c6582c3f63ee89b8c0ab17b2
pixelzoom <cmalley@pixelzoom.com>
2023-10-04 12:10:16 -0600

    TODO https://github.com/phetsims/my-solar-system/issues/213

--- js/model/Engine.ts
+++ js/model/Engine.ts
@@ -11,17 +11,26 @@ import Body from './Body.js';
 import solarSystemCommon from '../solarSystemCommon.js';
 
 export default abstract class Engine {
+
   // Array of gravitational interacting bodies
   protected bodies: ObservableArray<Body>;
 
   protected constructor( bodies: ObservableArray<Body> ) {
     this.bodies = bodies;
   }
+
+  //TODO https://github.com/phetsims/my-solar-system/issues/213 document
   public checkCollisions(): void {
     // no-op
   }
+
+  //TODO https://github.com/phetsims/my-solar-system/issues/213 document
   public abstract run( dt: number, updateProperties: boolean ): void;
+
+  //TODO https://github.com/phetsims/my-solar-system/issues/213 document
   public abstract update( bodies: ObservableArray<Body> ): void;
+
+  //TODO https://github.com/phetsims/my-solar-system/issues/213 document
   public abstract reset(): void;
 }
 
