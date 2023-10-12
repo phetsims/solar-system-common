e681c0ddb4493f973989d41c9efc0fb16c6a4684
pixelzoom <cmalley@pixelzoom.com>
2023-10-12 13:44:35 -0600

    cleanup

--- js/view/CueingArrowsNode.ts
+++ js/view/CueingArrowsNode.ts
@@ -40,7 +40,7 @@ export default class CueingArrowsNode extends Path {
 
     }, providedOptions );
 
-    super( createArrowsShape( options.bodyRadius, options.length ), options );
+    super( createArrowsShape( options.bodyRadius + 5, options.length ), options );
 
     this.localBoundsProperty.link( localBounds => {
       this.touchArea = localBounds.dilated( 5 );
@@ -58,12 +58,16 @@ const ARROW_SHAPE_OPTIONS = {
   tailWidth: 3
 };
 
-function createArrowsShape( radius: number, length: number ): Shape {
-  radius += 5;
-  const leftArrowShape = new ArrowShape( -radius, 0, -radius - length, 0, ARROW_SHAPE_OPTIONS );
-  const downArrowShape = new ArrowShape( 0, -radius, 0, -radius - length, ARROW_SHAPE_OPTIONS );
-  const upArrowShape = new ArrowShape( 0, radius, 0, radius + length, ARROW_SHAPE_OPTIONS );
-  const rightArrowShape = new ArrowShape( radius, 0, radius + length, 0, ARROW_SHAPE_OPTIONS );
+/**
+ * Creates the Shape for the cueing arrows.
+ * @param spacing - spacing between the tails of the arrows
+ * @param length - length of each arrow
+ */
+function createArrowsShape( spacing: number, length: number ): Shape {
+  const leftArrowShape = new ArrowShape( -spacing, 0, -spacing - length, 0, ARROW_SHAPE_OPTIONS );
+  const downArrowShape = new ArrowShape( 0, -spacing, 0, -spacing - length, ARROW_SHAPE_OPTIONS );
+  const upArrowShape = new ArrowShape( 0, spacing, 0, spacing + length, ARROW_SHAPE_OPTIONS );
+  const rightArrowShape = new ArrowShape( spacing, 0, spacing + length, 0, ARROW_SHAPE_OPTIONS );
   return Shape.union( [ leftArrowShape, downArrowShape, upArrowShape, rightArrowShape ] );
 }
 
