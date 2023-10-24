229770db1f563a8fab445c9a4ef69b781286bc9b
pixelzoom <cmalley@pixelzoom.com>
2023-10-24 12:47:01 -0600

    move unit tests into SolarSystemCommonConstants, https://github.com/phetsims/my-solar-system/issues/252

--- js/SolarSystemCommonConstants.ts
+++ js/SolarSystemCommonConstants.ts
@@ -79,6 +79,29 @@ const SolarSystemCommonConstants = {
   VELOCITY_TO_VIEW_MULTIPLIER: 50 * POSITION_MULTIPLIER / VELOCITY_MULTIPLIER
 };
 
+/**
+ * Sanity check for constants related to units.
+ */
+function testUnits(): void {
+
+  // Some example values for our test
+  const mass1 = 123; // kg
+  const mass2 = 456; // kg
+  const distance = 999; // m
+  const gravityForce = G_ACTUAL * mass1 * mass2 / ( distance * distance ); // N
+
+  // Convert to sim units
+  const mass1SimUnits = mass1 / MASS_MULTIPLIER;
+  const mass2SimUnits = mass2 / MASS_MULTIPLIER;
+  const distanceSimUnits = distance / METERS_PER_AU / POSITION_MULTIPLIER;
+  const gravityForceSimUnits = G * mass1SimUnits * mass2SimUnits / ( distanceSimUnits * distanceSimUnits );
+
+  // convert gravityForceSimUnits to SI
+  const convertedToSI = gravityForceSimUnits * MASS_MULTIPLIER * POSITION_MULTIPLIER * METERS_PER_AU / TIME_MULTIPLIER / TIME_MULTIPLIER / SECONDS_PER_YEAR / SECONDS_PER_YEAR;
+  assert && assert( Math.abs( gravityForce - convertedToSI ) < 1e-10, 'gravityForce and convertedToSI should be the same, within some small tolerance' );
+}
+testUnits();
+
 solarSystemCommon.register( 'SolarSystemCommonConstants', SolarSystemCommonConstants );
 export default SolarSystemCommonConstants;
 
