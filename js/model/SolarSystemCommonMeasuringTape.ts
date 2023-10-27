413b2a5bafcd4b82bdb5b9e54e0753daab29ad6c
pixelzoom <cmalley@pixelzoom.com>
2023-10-26 22:11:51 -0600

    feature measuringTape Properties, https://github.com/phetsims/my-solar-system/issues/237

--- js/model/SolarSystemCommonMeasuringTape.ts
+++ js/model/SolarSystemCommonMeasuringTape.ts
@@ -11,7 +11,7 @@ import Vector2 from '../../../dot/js/Vector2.js';
 import Vector2Property from '../../../dot/js/Vector2Property.js';
 import solarSystemCommon from '../solarSystemCommon.js';
 import PickRequired from '../../../phet-core/js/types/PickRequired.js';
-import { PhetioObjectOptions } from '../../../tandem/js/PhetioObject.js';
+import PhetioObject, { PhetioObjectOptions } from '../../../tandem/js/PhetioObject.js';
 import optionize from '../../../phet-core/js/optionize.js';
 
 type SelfOptions = {
@@ -21,26 +21,36 @@ type SelfOptions = {
 
 type SolarSystemCommonMeasuringTapeOptions = SelfOptions & PickRequired<PhetioObjectOptions, 'tandem'>;
 
-export default class SolarSystemCommonMeasuringTape {
+export default class SolarSystemCommonMeasuringTape extends PhetioObject {
 
   public readonly basePositionProperty: Property<Vector2>;
   public readonly tipPositionProperty: Property<Vector2>;
 
   public constructor( providedOptions: SolarSystemCommonMeasuringTapeOptions ) {
 
-    const options = optionize<SolarSystemCommonMeasuringTapeOptions, SelfOptions>()( {
+    const options = optionize<SolarSystemCommonMeasuringTapeOptions, SelfOptions, PhetioObjectOptions>()( {
+
+      // SelfOptions
       basePosition: new Vector2( 0, 0 ),
-      tipPosition: new Vector2( 0, 0 )
+      tipPosition: new Vector2( 0, 0 ),
+
+      // PhetioObjectOptions
+      phetioState: false,
+      phetioFeatured: true
     }, providedOptions );
+
+    super( options );
 
     this.basePositionProperty = new Vector2Property( options.basePosition, {
       units: 'AU',
-      tandem: options.tandem.createTandem( 'basePositionProperty' )
+      tandem: options.tandem.createTandem( 'basePositionProperty' ),
+      phetioFeatured: true
     } );
 
     this.tipPositionProperty = new Vector2Property( options.tipPosition, {
       units: 'AU',
-      tandem: options.tandem.createTandem( 'tipPositionProperty' )
+      tandem: options.tandem.createTandem( 'tipPositionProperty' ),
+      phetioFeatured: true
     } );
   }
 
