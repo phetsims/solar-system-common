2b2bbdd0e00edd80b8c6c80b055d45e9dfde50c5
phet-dev <phetadmin@gmail.com>
2024-01-30 03:18:37 -0700

    update copyright dates from daily grunt work

--- js/view/BodyNode.ts
+++ js/view/BodyNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * Visible Body Node that draws a sphere with size dependent on the Body's mass.
