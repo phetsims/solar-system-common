37f1f5df8d1fe73bd4a98841ccd6f36dccd8e475
phet-dev <phetadmin@gmail.com>
2024-02-14 03:26:40 -0700

    update copyright dates from daily grunt work

--- js/view/CueingArrowsNode.ts
+++ js/view/CueingArrowsNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * CueingArrowsNode is the cueing arrows used to indicate that something can be dragged in some direction.
