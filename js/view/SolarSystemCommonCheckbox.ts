96a646923d790b1977aa64b1b480b9f9b009cffc
phet-dev <phetadmin@gmail.com>
2024-03-23 03:28:03 -0600

    update copyright dates from daily grunt work

--- js/view/SolarSystemCommonCheckbox.ts
+++ js/view/SolarSystemCommonCheckbox.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * SolarSystemCommonCheckbox creates checkboxes with common options for the my-solar-system and keplers-laws sims.
