24f3ed9b5a1837b9340aa0e2e9b4b42335308e34
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:41 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for solar-system-common.
+ * ESLint configuration for solar-system-common.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
